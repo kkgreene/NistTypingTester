07e4780f2651b49bd48009c51f07d3a4f947f975
Matthew Kerr <kerrmj@gmail.com>
2013-12-05 20:35:24 -0500

    ??

--- newVersions/iOS6/TypingTester/TypingTester/ttSettingsViewController.h
+++ newVersions/iOS6/TypingTester/TypingTester/ttSettingsViewController.h
@@ -11,6 +11,8 @@
 
 @class ttSettingsViewController;
 
+
+// Delegate protocol for caller to implement to recieve notifications from this view contoller
 @protocol SettingsViewControllerDelegate <NSObject>
 
 -(void)SettingViewControllerDidCancel:(ttSettingsViewController*)controller;
@@ -21,10 +23,14 @@
 
 @interface ttSettingsViewController : UIViewController <SettingsDetailViewControllerDelegate>
 
+// The delegate to send messages to
 @property (nonatomic, weak) id <SettingsViewControllerDelegate> delegate;
 
+// Action for the cancel button
 -(IBAction)cancel:(id)sender;
+// Action for the save button
 -(IBAction)save:(id)sender;
+// Action for the background button
 -(IBAction)backgroundButtonPressed:(id)sender;
 
 @end
