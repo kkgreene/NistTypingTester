354c8f0552d6b5fa94f41af348b98042c332091e
Matthew Kerr <kerrmj@gmail.com>
2013-12-17 22:13:17 -0500

    Removed the separator lines in the table view on iPhone recall.  Added the next button as a table view row on the iPad recall screen.
    This may need to be reevaluated as the button will not be visible in cases when there are a large number of potential entries.

--- newVersions/iOS7/TypingTester/TypingTester/ttRecallTableViewController.h
+++ newVersions/iOS7/TypingTester/TypingTester/ttRecallTableViewController.h
@@ -9,10 +9,20 @@
 #import <UIKit/UIKit.h>
 
 @class ttSession;
+@class ttRecallTableViewController;
+
+@protocol RecallTableViewControllerDelegate <NSObject>
+
+-(void)RecallTableViewController:(ttRecallTableViewController*)controller didFinishWithValues:(NSString*)values;
+
+@end
 
 @interface ttRecallTableViewController : UITableViewController <UITextFieldDelegate>
 
 @property (nonatomic, weak) ttSession *session;
+@property (nonatomic, weak) id<RecallTableViewControllerDelegate> delegate;
+
+-(IBAction)done;
 
 -(void)hideKeyboard;
 -(NSString*) getStrings;
