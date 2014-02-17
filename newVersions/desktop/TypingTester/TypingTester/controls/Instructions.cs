8a33b975adc4ef1b7ba86bd0d46c15e8bc40acdb
Matt Kerr <matt.kerr@g2-inc.com>
2014-02-17 11:27:39 -0500

    Changed instructions screen loading to dynamically build the instructions file.

--- newVersions/desktop/TypingTester/TypingTester/controls/Instructions.cs
+++ newVersions/desktop/TypingTester/TypingTester/controls/Instructions.cs
@@ -38,10 +38,42 @@ namespace TypingTester.controls
             SetHeaderText("Instructions");
             SetEntityProgressVisibility(false);
             SetRoundProgresssVisibility(false);
-            FileStream source = new FileStream(@".\documents\instructions.html", FileMode.Open, FileAccess.Read);
-            webBrowser1.DocumentStream = source;
+            //FileStream source = new FileStream(@".\documents\instructions.html", FileMode.Open, FileAccess.Read);
+            //webBrowser1.DocumentStream = source;
+            webBrowser1.DocumentText = LoadHtml();
             Session.Instance.CurrentPhase = Constants.Phase.Instruction;
             Session.Instance.CurrentSubPhase = Constants.SubPhase.Unknown;
+        }
+
+        private string LoadHtml()
+        {
+            Options opt = Options.Instance;
+            StringBuilder sb = new StringBuilder();
+            string Header = string.Empty;
+            string FreePractice = string.Empty;
+            string ForcedPractice = string.Empty;
+            string Verify = string.Empty;
+            string Entry = string.Empty;
+            string Footer = string.Empty;
+
+            sb.AppendFormat(@"<html><head></head><body>");
+            Header = File.ReadAllText(@".\documents\instructionsHeader.fhtm");
+            if (!opt.disableFreePractice) FreePractice = File.ReadAllText(@".\documents\instructionsFreePractice.fhtm");
+            if (opt.ForcedPracticeRounds > 0) ForcedPractice = File.ReadAllText(@".\documents\instructionsForcedPractice.fhtm");
+            if (opt.VerifyRounds > 0) Verify = File.ReadAllText(@".\documents\instructionsVerify.fhtm");
+            Entry = File.ReadAllText(@".\documents\instructionsEntry.fhtm");
+            Footer = File.ReadAllText(@".\documents\instructionsFooter.fhtm");
+
+            if (!string.IsNullOrEmpty(Header)) sb.Append(Header);
+            if (!string.IsNullOrEmpty(FreePractice)) sb.Append(FreePractice);
+            if (!string.IsNullOrEmpty(ForcedPractice)) sb.Append(ForcedPractice);
+            if (!string.IsNullOrEmpty(Verify)) sb.Append(Verify);
+            if (!string.IsNullOrEmpty(Entry)) sb.Append(Entry);
+            if (!string.IsNullOrEmpty(Footer)) sb.Append(Footer);
+
+            sb.AppendFormat(@"</body></html>");
+
+            return sb.ToString();
         }
     }
 }
